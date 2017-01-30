c7f73120b0374dd12a60f633ba9e5c5de8f334e8
Chris Pierce <cpierce@csdurant.com>
2017-01-29 23:49:17 -0600

    Update to files for brew

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -7,12 +7,12 @@ brew install awscli
 brew install s3cmd
 brew install vim
 brew install the_silver_searcher
-brew install php70
-brew install php70-mcrypt
-brew install php70-intl
-brew install php70-xdebug
+brew install homebrew/php/php70
+brew install homebrew/php/php70-mcrypt
+brew install homebrew/php/php70-intl
+brew install homebrew/php/php70-xdebug
 brew install pwgen
-brew install phpunit
+brew install homebrew/php/phpunit
 brew install nmap
 brew cask install virtualbox
 brew cask install vagrant
