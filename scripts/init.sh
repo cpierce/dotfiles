127d2afb69f135e6b64518224138c5c751697f6b
Chris Pierce <cpierce@csdurant.com>
2017-06-01 17:22:24 -0500

    update for printer settings

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,3 +3,4 @@
 ./scripts/brew_init.sh
 ./scripts/vagrant_init.sh
 ./scripts/atom_init.sh
+./scripts/mac_init.sh
---
+++ scripts/mac_init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+defaults write com.google.Chrome DisablePrintPreview -boolean true
+
