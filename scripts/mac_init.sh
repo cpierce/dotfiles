82db2d4261682150fef40a8728dc725257589789
Chris Pierce <cpierce@csdurant.com>
2017-11-12 14:27:46 -0600

    update for cloud print

--- scripts/mac_init.sh
+++ scripts/mac_init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Disable Google Chrome print preview and use system default
-defaults write com.google.Chrome DisablePrintPreview -boolean true
+# I've turned this off because I recently bought a google cloud print ready
+# printer
+# defaults write com.google.Chrome DisablePrintPreview -boolean true
