fa3f1ade17853d13e47f90d102beb92ad221bc66
Chris Pierce <cpierce@csdurant.com>
2021-01-12 16:16:48 -0600

    update to zsh to use sudo

--- scripts/zsh_init.sh
+++ scripts/zsh_init.sh
@@ -7,6 +7,6 @@ cp .vimrc ~/
 sudo cp sudoers.d/* /etc/sudoers.d/
 mkdir -p ~/Workspace
 cp -R .ssh ~/
-chmod 700 ~/.ssh
-chown 600 ~/.ssh/id_rsa*
+sudo chmod 700 ~/.ssh
+sudo chown 600 ~/.ssh/id_rsa*
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
