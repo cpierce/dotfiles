e9b460b9f957a8b4a51fa45eeed083d565ac2378
Chris Pierce <cpierce@csdurant.com>
2021-03-16 17:02:49 -0500

    update

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -4,31 +4,30 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Now packages for we use with Brew
-brew install git
-brew install wget
-brew install tmate
-brew install jq
-brew install awscli
-brew install s3cmd
-brew install vim
-brew install the_silver_searcher
-brew install php@7.4
-pecl install xdebug
-brew install php-cs-fixer
-brew install pwgen
-brew install homebrew/php/phpunit
-brew install nmap
-brew install terraform
+brew install 1password-cli
 brew install ansible
-brew install shellcheck
-brew install watch
+brew install awscli
+brew install docker
+brew install git
+brew install jq
+brew install ngrok
+brew install nmap
 brew install node@12
-brew install packer
 brew install openconnect
-brew install --cask virtualbox
-brew install --cask vagrant
-brew install --cask ngrok
-brew install --cask 1password-cli
-brew install --cask docker
+brew install packer
+brew install php@7.4
+brew install php-cs-fixer
+brew install phpunit
+brew install powershell
+brew install pwgen
+brew install s3cmd
+brew install shellcheck
+brew install tmate
+brew install terraform
+brew install the_silver_searcher
+brew install vagrant
+brew install virtualbox
+brew install vim
+brew install watch
+brew install wget
 brew upgrade
-brew upgrade --cask
