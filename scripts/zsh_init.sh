4c3a8fe3ab1f9a3222133b26f194bbfb77a56b50
Chris Pierce <cpierce@csdurant.com>
2021-12-15 09:39:50 -0600

    new os updates

--- scripts/zsh_init.sh
+++ scripts/zsh_init.sh
@@ -7,7 +7,7 @@ cp .vimrc ~/
 sudo cp sudoers.d/* /etc/sudoers.d/
 mkdir -p ~/Workspace
 cp -R .ssh ~/
-sudo chown cpierce ~/.ssh/id_rsa*
+sudo chown cpierce ~/.ssh/id_*
 sudo chmod 700 ~/.ssh
-sudo chown 600 ~/.ssh/id_rsa*
+sudo chown 600 ~/.ssh/id_*
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
