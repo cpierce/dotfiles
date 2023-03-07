3b9f1aee2edb59a12b718f28f6d0b0f9c0143ac3
Chris Pierce <cpierce@csdurant.com>
2023-03-07 16:24:48 -0600

    disable autofill in favor of 1Password

--- scripts/mac_init.sh
+++ scripts/mac_init.sh
@@ -4,3 +4,7 @@
 # I've turned this off because I recently bought a google cloud print ready
 # printer
 # defaults write com.google.Chrome DisablePrintPreview -boolean true
+defaults write com.apple.Safari AutoFillCreditCardData -boolean false
+defaults write com.apple.Safari AutoFillFromAddressBook -boolean false
+defaults write com.apple.Safari AutoFillMiscellaneousForms -boolean false
+defaults write com.apple.Safari AutoFillPasswords -boolean false
