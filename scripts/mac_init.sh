5053d434a62503f3b7310e02d6b8d90a17730992
Chris Pierce <cpierce@csdurant.com>
2023-10-25 18:08:15 -0500

    dock settings moved over.

--- scripts/mac_init.sh
+++ scripts/mac_init.sh
@@ -12,31 +12,31 @@ defaults write com.apple.Safari AutoFillPasswords -boolean false
 
 # Finder: Settings
 # [x] Show all filename extensions
-# [ ] Show warning before changing an extension
-# [ ] Show warning before removing from iCloud Drive
-# [ ] Show warning before emptying the Trash
-# [x] Remove items from the Trash after 30 days
-# [x] Keep folders on top when sorting by name
-# [x] Show path bar
-# [x] Show status bar
-# [x] Show full path in finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# [ ] Show warning before changing an extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# [ ] Show warning before removing from iCloud Drive
 defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool false
+# [ ] Show warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
+# [x] Remove items from the Trash after 30 days
 defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+# [x] Keep folders on top when sorting by name
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
-defaults write com.apple.finder ShowPathbar -bool true
+# [x] Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
+# [x] Show full path in finder
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# [x] Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
 # [x] Show Hard Disks on Desktop
-# [x] Show External Disks on Desktop
-# [x] Show Removable Media on Desktop
-# [x] Show Mounted Servers on Desktop
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+# [x] Show External Disks on Desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+# [x] Show Removable Media on Desktop
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# [x] Show Mounted Servers on Desktop
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 
 # Sidebar: Settings
@@ -55,3 +55,9 @@ defaults write com.apple.finder ShowExternalHardDrivesOnSidebar -bool true
 defaults write com.apple.finder ShowHardDrivesOnSidebar -bool true
 defaults write com.apple.finder ShowMountedServersOnSidebar -bool true
 defaults write com.apple.finder ShowRemovableMediaOnSidebar -bool true
+
+# Dock: Settings
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock largesize -int 68
