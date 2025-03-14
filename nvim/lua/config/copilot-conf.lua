7e3624077c98bda77b3f4694d4c0470672b2842c
Chris Pierce <cpierce@csdurant.com>
2025-03-14 14:54:28 -0500

    update for copilot config broken out into my conf files

---
+++ nvim/lua/config/copilot-conf.lua
@@ -0,0 +1,17 @@
+return {
+  suggestion = {
+    enabled = false,
+    auto_trigger = true,
+    hide_during_completion = true,
+    keymap = {
+      accept = false,
+      next = '<M-]>',
+      prev = '<M-[>',
+    },
+  },
+  panel = { enabled = false },
+  filetypes = {
+    markdown = true,
+    help = true,
+  },
+}
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,21 +3,5 @@ return {
   cmd = 'Copilot',
   build = ':Copilot auth',
   event = 'BufReadPost',
-  opts = {
-    suggestion = {
-      enabled = false,
-      auto_trigger = true,
-      hide_during_completion = true,
-      keymap = {
-        accept = false,
-        next = '<M-]>',
-        prev = '<M-[>',
-      },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
+  opts = require('config.copilot-conf'),
 }
