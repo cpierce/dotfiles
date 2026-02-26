a03f270f6584b846fddaa66b1558b7cfd620cdc9
Chris Pierce <cpierce@csdurant.com>
2026-02-25 18:26:57 -0600

    Added semgrep

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -44,6 +44,7 @@ brew install sslscan
 brew install tmate
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
+brew install semgrep
 brew install sequel-ace
 brew install tree
 brew install yt-dlp
